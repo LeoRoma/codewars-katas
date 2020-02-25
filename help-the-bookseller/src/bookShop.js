e19c07a9f6a21e19caded7918cb7b0bc3fc672be
LeoRoma <xiajtn@gmail.com>
2020-02-25 13:49:42 +0000

    created a book object

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,13 +1,38 @@
 function bookShop(listOfArt, listOfCat) {
-  
-  let books = listOfArt.join('').split(' ');
-  let book = books[0];
-  let code = books[1];
-  let initialB = book[0]
-  console.log(listOfCat[0])
-  if (listOfCat[0] === initialB){
-    return (`(${initialB} : ${code})`)
+  let bookObject = {}
+
+  for (let i = 0; i < listOfArt.length; i++) {
+    let books = listOfArt[i].split(' ');
+    let book = books[0];
+    let code = books[1];
+    let initialBook = book[0]
+    let key = initialBook
+    bookObject[key] = code
+  };
+  // console.log(bookObject)
+
+  if(listOfArt.length < 2){
+    // console.log(bookObject[listOfCat])
+    console.log(Object.keys(bookObject))
+      // return(`(${initialB} : ${code})`)
   }
+  // if (listOfArt.length < 2) {
+  //   if (listOfCat[0] === initialB) {
+  //     return (`(${initialB} : ${code})`)
+  //   };
+  // }
+  // for (let i = 0; i < listOfCat.length; i++) {
+
+
+  //     console.log(initialBook[listOfCat[i]])
+
+  // }
 };
+
+
+
+
+
+
 
 module.exports = bookShop;
--- help-the-bookseller/test/bookShop.test.js
+++ help-the-bookseller/test/bookShop.test.js
@@ -8,7 +8,7 @@ describe('bookShop', () => {
     expect(bookShop(s, c)).toEqual('(A : 20)');
   });
 
-  xit('should return "A : 20, B : 100" with "ABART 20, BKWRK 100"', () => {
+  it('should return "A : 20, B : 100" with "ABART 20, BKWRK 100"', () => {
     s = ['ABART 20', 'BKWRK 100'];
     c = ['A', 'B'];
     expect(bookShop(s, c)).toEqual('(A : 20) - (B : 100)');
