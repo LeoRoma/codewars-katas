a87513f1c3636a4a727662935333ca8bec2b28ac
LeoRoma <xiajtn@gmail.com>
2020-02-25 14:38:36 +0000

    last case added

--- help-the-bookseller/test/bookShop.test.js
+++ help-the-bookseller/test/bookShop.test.js
@@ -25,4 +25,10 @@ describe('bookShop', () => {
     c = ['A'];
     expect(bookShop(s, c)).toEqual('(A : 20)');
   });
+
+  it('should return "(A : 20) - (B : 114) - (C : 50) - (W : 0)" with "ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"', () => {
+    s = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"];
+    c = ["A", "B", "C", "W"];
+    expect(bookShop(s, c)).toEqual('(A : 20) - (B : 114) - (C : 50) - (W : 0)');
+  });
 });
