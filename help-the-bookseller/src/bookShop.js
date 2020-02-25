6918b91d1000e4817d2bb218ff92ec1daa2680ff
LeoRoma <xiajtn@gmail.com>
2020-02-25 18:20:39 +0000

    created array with initials of books

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,24 +1,27 @@
 function bookShop(listOfArt, listOfCat) {
-  let bookList = []
+  let sortedBookList = listOfArt.sort()
+  let initialOfBookList = []
 
 
-  for (let i = 0; i < listOfArt.length; i++) {
-    let books = listOfArt[i].split(' ');
+  for (let i = 0; i < sortedBookList.length; i++) {
+    let books = sortedBookList[i].split(' ');
+    
     let book = books[0];
+    
     let code = books[1];
+    console.log(code)
     let initialOfBook = book[0]
-    // console.log(book)
-    console.log(initialOfBook.length)
-    bookList.push({ initOfBook: initialOfBook, code: code })
+  
+    initialOfBookList.push(`${initialOfBook} ${code}`)
   };
 
-  // console.log(bookList);
+  console.log(bookList);
 
-  var result = bookList.reduce(function(map, obj) {
-    map[obj.initOfBook] = obj.code;
-    return map;
-}, {});
-console.log(result)
+//   var result = bookList.reduce(function(map, obj) {
+//     map[obj.initOfBook] = obj.code;
+//     return map;
+// }, {});
+// console.log(result)
   // if (listOfArt.length < 2) {
   //   let keyBook = Object.keys(bookObject)
   //   let codeBook = bookObject[listOfCat]
--- help-the-bookseller/test/bookShop.test.js
+++ help-the-bookseller/test/bookShop.test.js
@@ -26,8 +26,8 @@ describe('bookShop', () => {
     expect(bookShop(s, c)).toEqual('(A : 20)');
   });
 
-  it('should return "(A : 20) - (B : 114) - (C : 50) - (W : 0)" with "ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"', () => {
-    s = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"];
+  it('should return "(A : 20) - (B : 114) - (C : 50) - (W : 0)" with "ABART 20", "BKWRK 25", "CDXEF 50", "BTSQZ 89", "DRTYM 60"', () => {
+    s = ["ABART 20", "BKWRK 25", "CDXEF 50", "BTSQZ 89", "DRTYM 60"];
     c = ["A", "B", "C", "W"];
     expect(bookShop(s, c)).toEqual('(A : 20) - (B : 114) - (C : 50) - (W : 0)');
   });
