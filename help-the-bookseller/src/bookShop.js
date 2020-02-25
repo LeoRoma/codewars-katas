3eec28fe2ae72012b72f6f883ce5316438e4b5c2
LeoRoma <xiajtn@gmail.com>
2020-02-25 15:55:22 +0000

    looking to add different value to the same key

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,27 +1,39 @@
 function bookShop(listOfArt, listOfCat) {
-  let bookObject = {}
+  let bookList = []
+
 
   for (let i = 0; i < listOfArt.length; i++) {
     let books = listOfArt[i].split(' ');
     let book = books[0];
     let code = books[1];
-    let initialBook = book[0]
-    let key = initialBook
-    bookObject[key] = code
+    let initialOfBook = book[0]
+    if(initialOfBook === initialOfBook){
+      console.log(initialOfBook)
+    }
+    console.log(initialOfBook)
+    bookList.push({ initOfBook: initialOfBook, code: code })
   };
-  if (listOfArt.length < 2) {
-    let keyBook = Object.keys(bookObject)
-    let codeBook = bookObject[listOfCat]
-    return (`(${keyBook} : ${codeBook})`)
-  } else {
-    let result = ""
-    for (let i = 0; i < listOfCat.length; i++) {
-      if (bookObject[listOfCat[i]]) {
-        result += `(${listOfCat[i]} : ${bookObject[listOfCat[i]]}) - `
-      }
-    };
-    return (result.slice(0, -3))
-  };
+
+  // console.log(bookList);
+
+  var result = bookList.reduce(function(map, obj) {
+    map[obj.initOfBook] = obj.code;
+    return map;
+}, {});
+console.log(result)
+  // if (listOfArt.length < 2) {
+  //   let keyBook = Object.keys(bookObject)
+  //   let codeBook = bookObject[listOfCat]
+  //   return (`(${keyBook} : ${codeBook})`)
+  // } else {
+  //   let result = ""
+  //   for (let i = 0; i < listOfCat.length; i++) {
+  //     if (bookObject[listOfCat[i]]) {
+  //       result += `(${listOfCat[i]} : ${bookObject[listOfCat[i]]}) - `
+  //     }
+  //   };
+  //   return (result.slice(0, -3))
+  // };
 };
 
 
