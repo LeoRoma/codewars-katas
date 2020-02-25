b2cd1728e57f806d1580d14eda92ffc937d2d600
LeoRoma <xiajtn@gmail.com>
2020-02-25 12:43:10 +0000

    change code for the first test

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,5 +1,13 @@
-function bookShop(books){
-  return ('A : 20')
+function bookShop(listOfArt, listOfCat) {
+  
+  let books = listOfArt.join('').split(' ');
+  let book = books[0];
+  let code = books[1];
+  let initialB = book[0]
+  console.log(listOfCat[0])
+  if (listOfCat[0] === initialB){
+    return (`(${initialB} : ${code})`)
+  }
 };
 
 module.exports = bookShop;
--- help-the-bookseller/test/bookShop.test.js
+++ help-the-bookseller/test/bookShop.test.js
@@ -2,7 +2,15 @@ const bookShop = require('../src/bookShop.js');
 
 describe('bookShop', () => {
 
-  it('should return ABART 20', () => {
-    expect(bookShop('ABART 20')).toEqual('A : 20')
+  it('should return "A : 20" with "ABART 20"', () => {
+    s = ['ABART 20'];
+    c = ['A']
+    expect(bookShop(s, c)).toEqual('(A : 20)');
   });
+
+  xit('should return "A : 20, B : 100" with "ABART 20, BKWRK 100"', () => {
+    s = ['ABART 20', 'BKWRK 100'];
+    c = ['A', 'B'];
+    expect(bookShop(s, c)).toEqual('(A : 20) - (B : 100)');
+  })
 });
