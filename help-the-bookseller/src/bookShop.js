8f58a50e7ee95b0fba4c72b60152cce7c2773475
LeoRoma <xiajtn@gmail.com>
2020-02-26 13:00:10 +0000

    almost at the solution

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,6 +1,7 @@
 function bookShop(listOfArt, listOfCat) {
   let sortedBookList = listOfArt.sort();
   let bookWithCodeList = [];
+  let sumOfCodeList = {};
 
   for (let i = 0; i < sortedBookList.length; i++) {
     let books = sortedBookList[i].split(' ');
@@ -10,61 +11,31 @@ function bookShop(listOfArt, listOfCat) {
     bookWithCodeList.push({ initial: initialOfBook, code: code });
   };
 
-  // console.log(bookWithCodeList);
-
-  // const sumOfCode = bookWithCodeList.reduce((result, item) => {
-  //   listOfCat.forEach(initial => {
-  //     if (!result[initial]) {
-  //       result[initial] = 0
-  //     }
-  //     console.log(initial)
-  //     console.log(item)
-  //   });
-  //   console.log(result)
-  // }, {});
-
-  var holder = {};
-
-  bookWithCodeList.forEach(function (d) {
-    let code = parseInt(d.code)
-    console.log(code)
-    if (holder.hasOwnProperty(d.initial)) {
-
-
-      holder[d.initial] = holder[d.initial] + code;
+  bookWithCodeList.forEach(function (book) {
+    let code = parseInt(book.code)
+    let initial = book.initial
+    if (sumOfCodeList.hasOwnProperty(initial)) {
+      sumOfCodeList[initial] = sumOfCodeList[initial] + code;
     } else {
-      holder[d.initial] = code;
+      sumOfCodeList[initial] = code;
     }
   });
-  console.log(holder)
-  var obj2 = [];
 
-  for (var prop in holder) {
-    obj2.push({ name: prop, value: holder[prop] });
-  }
+  console.log(sumOfCodeList)
 
-
-  console.log(obj2)
-
-
-  //   var result = bookList.reduce(function(map, obj) {
-  //     map[obj.initOfBook] = obj.code;
-  //     return map;
-  // }, {});
-  // console.log(result)
-  // if (listOfArt.length < 2) {
-  //   let keyBook = Object.keys(bookObject)
-  //   let codeBook = bookObject[listOfCat]
-  //   return (`(${keyBook} : ${codeBook})`)
-  // } else {
-  //   let result = ""
-  //   for (let i = 0; i < listOfCat.length; i++) {
-  //     if (bookObject[listOfCat[i]]) {
-  //       result += `(${listOfCat[i]} : ${bookObject[listOfCat[i]]}) - `
-  //     }
-  //   };
-  //   return (result.slice(0, -3))
-  // };
+  if (sumOfCodeList.length < 2) {
+    let keyBook = Object.keys(bookObject)
+    let codeBook = bookObject[listOfCat]
+    return (`(${keyBook} : ${codeBook})`)
+  } else {
+    let result = ""
+    for (let i = 0; i < listOfCat.length; i++) {
+      if (sumOfCodeList[listOfCat[i]]) {
+        result += `(${listOfCat[i]} : ${sumOfCodeList[listOfCat[i]]}) - `
+      }
+    };
+    return (result.slice(0, -3))
+  };
 };
 
 
