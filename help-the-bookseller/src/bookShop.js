2bc8306ba774147ff78b12ae1272e254977c0dd8
LeoRoma <xiajtn@gmail.com>
2020-02-26 12:50:29 +0000

    sum of values for the same key

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,27 +1,57 @@
 function bookShop(listOfArt, listOfCat) {
-  let sortedBookList = listOfArt.sort()
-  let initialOfBookList = []
-
+  let sortedBookList = listOfArt.sort();
+  let bookWithCodeList = [];
 
   for (let i = 0; i < sortedBookList.length; i++) {
     let books = sortedBookList[i].split(' ');
-    
     let book = books[0];
-    
     let code = books[1];
-    console.log(code)
     let initialOfBook = book[0]
-  
-    initialOfBookList.push(`${initialOfBook} ${code}`)
+    bookWithCodeList.push({ initial: initialOfBook, code: code });
   };
 
-  console.log(bookList);
+  // console.log(bookWithCodeList);
 
-//   var result = bookList.reduce(function(map, obj) {
-//     map[obj.initOfBook] = obj.code;
-//     return map;
-// }, {});
-// console.log(result)
+  // const sumOfCode = bookWithCodeList.reduce((result, item) => {
+  //   listOfCat.forEach(initial => {
+  //     if (!result[initial]) {
+  //       result[initial] = 0
+  //     }
+  //     console.log(initial)
+  //     console.log(item)
+  //   });
+  //   console.log(result)
+  // }, {});
+
+  var holder = {};
+
+  bookWithCodeList.forEach(function (d) {
+    let code = parseInt(d.code)
+    console.log(code)
+    if (holder.hasOwnProperty(d.initial)) {
+
+
+      holder[d.initial] = holder[d.initial] + code;
+    } else {
+      holder[d.initial] = code;
+    }
+  });
+  console.log(holder)
+  var obj2 = [];
+
+  for (var prop in holder) {
+    obj2.push({ name: prop, value: holder[prop] });
+  }
+
+
+  console.log(obj2)
+
+
+  //   var result = bookList.reduce(function(map, obj) {
+  //     map[obj.initOfBook] = obj.code;
+  //     return map;
+  // }, {});
+  // console.log(result)
   // if (listOfArt.length < 2) {
   //   let keyBook = Object.keys(bookObject)
   //   let codeBook = bookObject[listOfCat]
