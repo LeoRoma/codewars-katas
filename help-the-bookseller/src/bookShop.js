4f25983643b17460cc829795afb9a25d98e70f17
LeoRoma <xiajtn@gmail.com>
2020-02-29 00:37:20 +0000

    converted listOfArt array into key/value pairs object

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -1,48 +1,53 @@
 function bookShop(listOfArt, listOfCat) {
   let sortedBookList = listOfArt.sort();
   let bookWithCodeList = [];
-  let sumOfCodeList = {};
-  let result = ""
+  // let sumOfCodeList = {};
+  let result = "";
+
 
   for (let i = 0; i < sortedBookList.length; i++) {
+    let bookWithCode = {};
     let books = sortedBookList[i].split(' ');
     let book = books[0];
     let code = books[1];
     let initialOfBook = book[0]
-    bookWithCodeList.push({ initial: initialOfBook, code: code });
+    bookWithCode[initialOfBook] = code
+    bookWithCodeList.push(bookWithCode)
   };
 
-  bookWithCodeList.forEach(function (book) {
-    let code = parseInt(book.code)
-    let initial = book.initial
-    if (sumOfCodeList.hasOwnProperty(initial)) {
-      sumOfCodeList[initial] = sumOfCodeList[initial] + code;
-    } else {
-      sumOfCodeList[initial] = code;
-    }
-  });
+  console.log(bookWithCodeList)
+
+  // bookWithCodeList.forEach(function (book) {
+  //   let code = parseInt(book.code)
+  //   let initial = book.initial
+  //   if (sumOfCodeList.hasOwnProperty(initial)) {
+  //     sumOfCodeList[initial] = sumOfCodeList[initial] + code;
+  //   } else {
+  //     sumOfCodeList[initial] = code;
+  //   }
+  // });
 
   // if (sumOfCodeList.length < 2) {
   //   let keyBook = Object.keys(bookObject)
   //   let codeBook = bookObject[listOfCat]
   //   return (`(${keyBook} : ${codeBook})`)
   // } else {
-    // const sumOfCodeList = bookWithCodeList.reduce((result, item) => {
-    //   listOfCat.forEach(key => {
-    //     // console.log(key)
-    //     if (!result[key]) {
-    //       result[key] = 0;
-    //     }
-    //     // console.log(item[key])
-    //     let value = parseInt(item[key], 10);
-    //     result[key] += value;
-    //   });
+  const sumOfCodeList = bookWithCodeList.reduce((result, item) => {
+    listOfCat.forEach(key => {
+      // console.log(key)
+      if (!result[key]) {
+        result[key] = 0;
+      }
+      // console.log(item[key])
+      let value = parseInt(item[key], 10);
+      result[key] += value;
+    });
 
-    //   return result;
-    // }, {});
+    return result;
+  }, {});
     console.log(sumOfCodeList)
-  };
-  // return (result.slice(0, -3))
+};
+// return (result.slice(0, -3))
 
 
 
