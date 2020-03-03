032b54401e01b79de1a2fa7729731b6454fe4f40
LeoRoma <xiajtn@gmail.com>
2020-03-03 14:43:25 +0000

    kata completed

--- help-the-bookseller/src/bookShop.js
+++ help-the-bookseller/src/bookShop.js
@@ -3,7 +3,7 @@ function bookShop(listOfArt, listOfCat) {
   let bookWithCodeList = [];
 
   let sumOfCodeObject = {};
-  let resultObject = {};
+  let result = '';
 
   for (let i = 0; i < sortedBookList.length; i++) {
     let bookWithCode = {};
@@ -11,12 +11,8 @@ function bookShop(listOfArt, listOfCat) {
     let book = books[0];
     let code = Number.parseInt(books[1]);
     let initialOfBook = book[0].toString()
-    // console.log(initialOfBook)
-    // bookWithCode[initialOfBook] = code
-    // bookWithCodeList.push(bookWithCode)
     bookWithCodeList.push({ initial: initialOfBook, code: code })
   };
-  // console.log(bookWithCodeList)
 
   bookWithCodeList.forEach(item => {
     let initial = item.initial;
@@ -28,20 +24,10 @@ function bookShop(listOfArt, listOfCat) {
     }
   });
 
-  console.log(sumOfCodeObject)
-
-
-  // if (sumOfCodeList.length < 2) {
-  //   let keyBook = Object.keys(bookObject)
-  //   let codeBook = bookObject[listOfCat]
-  //   return (`(${keyBook} : ${codeBook})`)
-  // } else {
-
   const resultList = listOfCat.reduce(function (acc, curr) {
     acc[curr] = 0;
     return acc;
   }, {});
-  console.log(resultList)
 
   for (var initialCat in resultList) {
     let codeCat = resultList[initialCat];
@@ -53,15 +39,16 @@ function bookShop(listOfArt, listOfCat) {
         resultList[initialCat] 
       }
     }
-    // console.log(initialCat, resultList[initialCat])
-  }
-  console.log(resultList)
+  };
 
-
-
-  // console.log(sumOfCodeList)
+  for(var resultInitial in resultList){
+    let resulCode = resultList[resultInitial];
+    result += `(${resultInitial} : ${resulCode}) - `
+  };
+  // console.log(result.slice(0, -3))
+  return (result.slice(0, -3))
 };
-// return (result.slice(0, -3))
+
 
 
 
